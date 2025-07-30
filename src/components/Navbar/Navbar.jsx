6a502436108aab7772836893ec33fe3f3b62eb4e
Yogita <theweridyoyo@gmail.com>
2025-07-30 12:49:28 +0530

    Contact Us Section.
    Signed-off-by: Yogita <theweridyoyo@gmail.com>

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
-import logo from '../../assets/logo.png';
-import './Navbar.css';
 import { Link } from 'react-scroll';
+import logo from '../../assets/logo.png';
+import menu_icon from '../../assets/menu-icon.png';
+import './Navbar.css';
 
 const Navbar = () => {
+
 
   const [sticky, setSticky]=useState(false);
 
@@ -11,11 +13,16 @@ const Navbar = () => {
     window.addEventListener('scroll',()=> {
        setSticky(window.scrollY> 50);
     })
-  },[])
+  },[]);
+  const [mobileMenu,setMobileMenu]=useState(false);
+  const toggleMenu =()=>{
+    setMobileMenu(!mobileMenu);
+
+  }
   return (
    <nav className= {`container ${sticky? `dark-nav`:''}`}>
     <img src ={logo} alt ="logo" className='logo'/>
-    <ul>
+    <ul className={mobileMenu ?'':'hide-mobile-menu'}>
         <li><Link to ='hero' smooth={true}offset={0} duration={500} >Home</Link></li>
          <li><Link to ='program' smooth={true}offset={-260} duration={500} >Program</Link></li>
           <li><Link to ='about' smooth={true}offset={-150} duration={500} >About us</Link></li>
@@ -23,6 +30,7 @@ const Navbar = () => {
            <li><Link to ='testimonials' smooth={true}offset={-260} duration={500} >Testimonial</Link></li>
            <li><button className='btn'>Contact us</button></li>
     </ul>
+    <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
    </nav>
   )
 }
